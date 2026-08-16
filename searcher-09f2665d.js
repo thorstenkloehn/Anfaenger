c06f0a6a25b76f406017cb656278f5189a59cc4d
Thorsten Klöhn <thorstenkloehn@gmail.com>
2026-08-16 12:12:20 +0200

    Updates

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-99b8ab81.js',
+                path_to_root + 'searchindex-f2a1182d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
